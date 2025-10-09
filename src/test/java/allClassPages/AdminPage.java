8236ec81d4296cf8162bb106970d049850354279
gondaliyaharsh1109 <gondaliyaharsh50@gmail.com>
2025-10-09 12:29:06 +0530

    added files

--- src/test/java/allClassPages/AdminPage.java
+++ src/test/java/allClassPages/AdminPage.java
@@ -6,10 +6,19 @@ import org.testng.Assert;
 
 public class AdminPage extends BasePage{
     By firstSearchTab = By.xpath("(//li[@class='oxd-main-menu-item-wrapper'])[1]");
-    By verifyAdminText = By.xpath("//div[contains(@class,'oxd-topbar-header')]//span[contains(@class,'oxd-topbar')]");
+    By verifySystemUsersText = By.xpath("//div[contains(@class,'oxd-table-filter-header-title')]");
     By clickAddBtn = By.xpath("//button[contains(normalize-space(),'Add')]");
     By verifyAddUserText = By.xpath("//h6[contains(@class,'oxd-text oxd-text--h6 orangehrm-main-title')]");
     By clickUserRole = By.xpath("(//div[contains(@class,'oxd-select-text-input')])[1]");
+    By selectAdminRole = By.xpath("//div[contains(@role,'listbox')]//span[contains(text(),'Admin')]");
+    By employeeNameInputField = By.xpath("//input[@placeholder='Type for hints...']");
+    By selectEmployeeOption = By.xpath("//div[@role='option']//span[normalize-space()='Orange Test']");
+    By clickStatus = By.xpath("(//div[contains(@class,'oxd-select-text-input')])[2]");
+    By selectStatus = By.xpath("//div[contains(@role,'listbox')]//span[contains(text(),'Enabled')]");
+    By usernameInputField = By.xpath("(//input[contains(@class,'oxd-input oxd-input--active')])[2]");
+    By passwordInputField = By.xpath("(//input[@type='password'])[1]");
+    By confirmPasswordInputField = By.xpath("(//input[@type='password'])[2]");
+    By clickSubmitBtn = By.xpath("//button[contains(normalize-space(),'Save')]");
 
     public AdminPage(WebDriver driver){
         super(driver);
@@ -18,8 +27,12 @@ public class AdminPage extends BasePage{
         DashboardPage dashboardPage = new DashboardPage(driver);
         dashboardPage.verifySearchFunctionalityInToggle("ad","Admin");
         waitForElement(firstSearchTab).click();
-        String actualAdminText = waitForElement(verifyAdminText).getText();
-        String expectedAdminText = "Admin";
-        Assert.assertEquals(actualAdminText,expectedAdminText);
+        String actualSystemUsersText = waitForElement(verifySystemUsersText).getText();
+        String expectedSystemUsersText = "System Users";
+        Assert.assertEquals(actualSystemUsersText,expectedSystemUsersText);
+        waitForElement(clickAddBtn).click();
+        String actualAddUserText = waitForElement(verifyAddUserText).getText();
+        String expectedAddUserText = "Add User";
+        Assert.assertEquals(actualAddUserText,expectedAddUserText);
     }
 }
